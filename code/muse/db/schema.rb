04105543aa58660cb00f5c07e168a90a4b247490
Ali <aaljamid@emich.edu>
2020-03-30 16:04:44 +0300

    created the artist model seeded the database

--- code/muse/db/schema.rb
+++ code/muse/db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_30_121326) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "hometown"
+    t.string "img"
+    t.integer "albums"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
